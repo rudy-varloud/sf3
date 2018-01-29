747131c421111afbd7ea4a45725098b5ab872604
Boris Guéry <guery.b@gmail.com>
2018-01-29 14:42:02 +0100

    feat(sales): Simple form to fetch data from request

--- src/AppBundle/Controller/SalesController.php
+++ src/AppBundle/Controller/SalesController.php
@@ -10,6 +10,14 @@ class SalesController extends Controller
 {
     public function submitTicketAction(Request $request): Response
     {
-        return $this->render('@App/Sales/submit_ticket.html.twig', []);
+        [$name, $date, $description] = [
+            $request->request->get('event_name'),
+            $request->request->get('event_date'),
+            $request->request->get('event_description'),
+        ];
+
+        var_dump($name, $date, $description);
+
+        return $this->render('@App/Sales/submit_ticket.html.twig');
     }
 }
