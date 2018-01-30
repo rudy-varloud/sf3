e556cabab6b91708bc27c254bc23e42fc2368b86
Boris Guéry <guery.b@gmail.com>
2018-01-30 10:35:24 +0100

    feat(members): Set up member factory from sign up form

---
+++ src/AppBundle/Utils/MemberFactory.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Boris Guéry <guery.b@gmail.com>
+ */
+
+namespace AppBundle\Utils;
+
+use AppBundle\Forms\MemberSignUp;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Tiquette\Domain\Email;
+use Tiquette\Domain\EncodedPassword;
+use Tiquette\Domain\Member;
+
+class MemberFactory
+{
+    private $passwordEncoder;
+
+    public function __construct(PasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function fromSignUp(MemberSignUp $signUp): Member
+    {
+        $salt = base64_encode(random_bytes(256));
+        $rawEncodedPassword = $this->passwordEncoder->encodePassword(
+            $signUp->password,
+            $salt
+        );
+
+        return Member::signUp(
+            new Email($signUp->email),
+            $signUp->nickname,
+            new EncodedPassword($rawEncodedPassword, $salt)
+        );
+    }
+}
